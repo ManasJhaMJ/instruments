fc56310b9c9d3940dd39d19c7fa6447f75165847
Manas Jha <jhamanas939@gmail.com>
2023-07-31 21:29:23 +0530

    fix button placement and sound

--- instruments/drum.js
+++ instruments/drum.js
@@ -1,8 +1,8 @@
-var numberOfDrumButtons = document.querySelectorAll(".button").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
-  document.querySelectorAll(".button")[i].addEventListener("click", function() {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
     var buttonInnerHTML = this.innerHTML;
 
@@ -14,7 +14,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 }
 
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keypress", function (event) {
 
   makeSound(event.key);
 
@@ -26,39 +26,60 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
+
     case "y":
-      var tom1 = new Audio("../sounds/drum/crash.mp3");
-      tom1.play();
-      break;
-
-    case "u":
-      var tom2 = new Audio("../sounds/drum/ride.mp3");
-      tom2.play();
-      break;
-
-    case "x":
-      var tom3 = new Audio('../sounds/drum/bass.mp3');
-      tom3.play();
-      break;
-
-    case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-      break;
-
-    case "j":
-      var snare = new Audio('sounds/snare.mp3');
-      snare.play();
-      break;
-
-    case "k":
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio("../sounds/drum/crash.mp3");
       crash.play();
       break;
 
-    case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
+    case "u":
+      var ride = new Audio("../sounds/drum/ride.mp3");
+      ride.play();
+      break;
+
+    case "x":
+      var bass = new Audio('../sounds/drum/bass.mp3');
+      bass.play();
+      break;
+
+    case "e":
+      var hihatopen = new Audio('../sounds/drum/hihat-open.mp3');
+      hihatopen.play();
+      break;
+
+    case "r":
+      var hihat = new Audio('../sounds/drum/hihat.mp3');
+      hihat.play();
+      break;
+
+    case "c":
+      var hihatfoot = new Audio('../sounds/drum/hihat-foot.mp3');
+      hihatfoot.play();
+      break;
+
+    case "g":
+      var tom1 = new Audio('../sounds/drum/tom1.mp3');
+      tom1.play();
+      break;
+
+    case "h":
+      var tom2 = new Audio('../sounds/drum/tom2.mp3');
+      tom2.play();
+      break;
+
+    case "j":
+      var floortom = new Audio('../sounds/drum/floor-tom.mp3');
+      floortom.play();
+      break;
+
+    case "s":
+      var snaredrum = new Audio('../sounds/drum/share-drum.mp3');
+      snaredrum.play();
+      break;
+
+    case "d":
+      var snarestick = new Audio('../sounds/drum/share-stick.mp3');
+      snarestick.play();
       break;
 
 
@@ -74,7 +95,7 @@ function buttonAnimation(currentKey) {
 
   activeButton.classList.add("pressed");
 
-  setTimeout(function() {
+  setTimeout(function () {
     activeButton.classList.remove("pressed");
   }, 100);
 
